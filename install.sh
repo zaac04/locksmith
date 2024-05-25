9493bc3d388b6828b56885362e4bec0067e6b742
issac <issacthomasvarghese@gmail.com>
2024-05-25 14:52:58 +0530

    Added : install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+url="https://github.com/zaac04/locksmith/releases/download/v1.0/locksmith"
+wget $url
+binary_destination_folder="/usr/local/bin"
+sudo cp locksmith $binary_destination_folder
+sudo chmod +x "$binary_destination_folder/locksmith"
+sudo locksmith completion bash >> /etc/bash_completion.d/locksmith.sh
+sudo echo "source /etc/bash_completion.d/locksmith.sh" >> ~/.bashrc
+. ~/.bashrc
+
